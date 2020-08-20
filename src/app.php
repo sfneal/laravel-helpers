413456232e26f1f74e9b12724c30971daef7c41d
Stephen Neal <stephen.neal14@gmail.com>
2020-08-20 12:09:49 -0400

    Apply fixes from StyleCI (#1)

--- src/app.php
+++ src/app.php
@@ -2,31 +2,31 @@
 
 use Illuminate\Support\Facades\Cache;
 
-
 /**
- * Return the alphabet in array form
+ * Return the alphabet in array form.
  *
  * @return array
  */
-function alphabet(): array {
+function alphabet(): array
+{
     return Cache::rememberForever('alphabet', function () {
         return range('A', 'Z');
     });
 }
 
 /**
- * Return the index of a letter in the alphabet
+ * Return the index of a letter in the alphabet.
  *
  * @param int $index
  * @return string
  */
-function alphabetIndex(int $index): string {
+function alphabetIndex(int $index): string
+{
     return alphabet()[$index];
 }
 
-
 /**
- * Retrieve a class's short name (without namespace)
+ * Retrieve a class's short name (without namespace).
  *
  * @param $class
  * @param bool $short Full name or short name
@@ -40,15 +40,14 @@ function getClassName($class, $short = false, string $default = null): string
         return (new ReflectionClass($class))->{$short ? 'getShortName' : 'getName'}();
     }
 
-        // Return $default
+    // Return $default
     catch (ReflectionException $e) {
         return $default;
     }
 }
 
-
 /**
- * Determine if the Application is running in a 'production' environment
+ * Determine if the Application is running in a 'production' environment.
  *
  * @return bool
  */
@@ -57,9 +56,8 @@ function isProductionEnvironment(): bool
     return env('APP_ENV') == 'production';
 }
 
-
 /**
- * Determine if the Application is running in a 'development' environment
+ * Determine if the Application is running in a 'development' environment.
  *
  * @return bool
  */
