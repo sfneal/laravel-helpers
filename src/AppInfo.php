22647456ea33c7b89fa69829fe98824c95e7db59
Stephen Neal <stephen.neal14@gmail.com>
2021-03-10 17:24:14 -0500

    FIX call to `isEnvironmentDevelopment()` method in `version()`

--- src/AppInfo.php
+++ src/AppInfo.php
@@ -25,7 +25,7 @@ class AppInfo
     {
         return Cache::rememberForever(self::cacheKey('version'), function () {
             // toto: refactor environment method to AppInfo
-            return trim(config('app-info.version').(LaravelHelpers::isDevelopmentEnvironment() ? ' (dev)' : ''));
+            return trim(config('app-info.version').(self::isEnvironmentDevelopment() ? ' (dev)' : ''));
         });
     }
 
