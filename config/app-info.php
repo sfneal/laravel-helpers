e2c64d3de03ab954d644a4eed9f953765739a5c0
Stephen Neal <stephen.neal14@gmail.com>
2021-03-10 15:09:13 -0500

    MAKE app-info.php config file that's published by the AppInfoServiceProvider

---
+++ config/app-info.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Version
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to set your application's version that can be
+    | used within your application
+    |
+    | Supported: string|null (example: "1.2.4")
+    |
+    */
+    'version' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Changelog Path
+    |--------------------------------------------------------------------------
+    |
+    | // todo: add support for markdowns
+    | Set the path to a txt file changelog.
+    |
+    | Supported: string|null
+    |
+    */
+    'changelog_path' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Prefix
+    |--------------------------------------------------------------------------
+    |
+    | Prefix when generating cache key's for storing application info.
+    |
+    | Supported: string
+    |
+    */
+    'cache_prefix' => 'app',
+];
