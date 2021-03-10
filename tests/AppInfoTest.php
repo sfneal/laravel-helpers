02b755a211ae85c9bfe4e30e80519a5c3de50dcb
Stephen Neal <stephen.neal14@gmail.com>
2021-03-10 15:32:16 -0500

    ADD `version` & `versionChanges` test methods

--- tests/AppInfoTest.php
+++ tests/AppInfoTest.php
@@ -2,11 +2,38 @@
 
 namespace Sfneal\Helpers\Laravel\Tests;
 
+use Sfneal\Helpers\Laravel\AppInfo;
+
 class AppInfoTest extends TestCase
 {
     /** @test */
-    public function alphabet()
+    public function version()
     {
+        $expected = '1.7.2';
+        $output = AppInfo::version();
 
+        $this->assertIsString($output);
+        $this->assertEquals($expected, $output);
+    }
+
+
+    /** @test */
+    public function versionChanges()
+    {
+        $expected = [
+            'date' => '2020-12-01',
+            'changes' => [
+                'cut support for php5.5 & below',
+            ],
+        ];
+
+        $version = '0.4.0';
+        $output = AppInfo::versionChanges($version);
+
+        $this->assertIsArray($output);
+        $this->assertIsString($output['date']);
+        $this->assertIsArray($output['changes']);
+        $this->assertEquals($expected['date'], $output['date']);
+        $this->assertEquals($expected['changes'], $output['changes']);
     }
 }
